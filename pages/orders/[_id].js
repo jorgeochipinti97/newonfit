424382faeb1eb3e38a34169cc92fda8671b88e66
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-02-14 17:57:23 -0300

    2

--- pages/orders/[_id].js
+++ pages/orders/[_id].js
@@ -17,26 +17,58 @@ const OrdersPage = () => {
   return (
     <ShopLayout>
       <div style={{ minHeight: "100vh", paddingTop: "100px" }}>
-        <p style={{ textAlign: "center", fontSize: "30px",marginRight:'2px',marginLeft:'2px' }}>
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "30px",
+            marginRight: "2px",
+            marginLeft: "2px",
+          }}
+        >
           ¡Muchas gracias por confiar en nosotros!
         </p>
         {order && (
-          <div style={{ marginTop: "50px",  }}>
-            <p style={{ fontWeight: "400",fontSize:'20px', marginTop:'10px',textAlign:'center',marginBottom:'10px' }}>
-              ID de la orden: <span style={{fontWeight:'600'}}>{order._id} </span>
+          <div style={{ marginTop: "50px" }}>
+            <p
+              style={{
+                fontWeight: "400",
+                fontSize: "20px",
+                marginTop: "10px",
+                textAlign: "center",
+                marginBottom: "10px",
+              }}
+            >
+              ID de la orden:{" "}
+              <span style={{ fontWeight: "600" }}>{order._id} </span>
             </p>
-            <p style={{ fontWeight: "400",fontSize:'20px', marginTop:'10px',textAlign:'center',marginBottom:'10px' }}>
-              Codigo de seguimiento : <br/><span style={{fontWeight:'600'}}> {order.codGestion} </span>
+            <p
+              style={{
+                fontWeight: "400",
+                fontSize: "20px",
+                marginTop: "10px",
+                textAlign: "center",
+                marginBottom: "10px",
+              }}
+            >
+              Codigo de seguimiento : <br />
+              <span style={{ fontWeight: "600" }}> {order.codGestion} </span>
             </p>
-            <div style={{      flexWrap:'wrap', display:'flex', alignItems:'center', justifyContent:'center'}}>
+            <div
+              style={{
+                flexWrap: "wrap",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               {order.orderItems.map((e) => (
                 <div
+                  key={e._id}
                   style={{
                     display: "flex",
                     flexDirection: "column",
                     alignItems: "center",
-                    marginTop:'50px',
-              
+                    marginTop: "50px",
                   }}
                 >
                   <img src={e.image} width={200} />
@@ -46,7 +78,6 @@ const OrdersPage = () => {
                   </p>
                 </div>
               ))}
-         
             </div>
           </div>
         )}
